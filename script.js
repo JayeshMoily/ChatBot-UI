91fd88e233f710fca23f51a3fef2d2daf9cadd0f
Jayesh Moily <64909841+JayeshMoily@users.noreply.github.com>
2020-09-17 14:33:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+setTimeout(function showdiv() {
+  document.getElementById("p1").style.visibility = "visible";
+}, 700);
+
+
+setTimeout(function showdiv() {
+  document.getElementById("p2").style.visibility = "visible";
+}, 700)
+
+setTimeout(function showdiv() {
+  document.getElementById("p3").style.visibility = "visible";
+}, 2000)
+
+setTimeout(function showdiv() {
+  document.getElementById("p4").style.visibility = "visible";
+}, 2000)
+
+setTimeout(function showdiv() {
+  document.getElementById("p5").style.visibility = "visible";
+}, 3500)
+
+setTimeout(function showdiv() {
+  document.getElementById("p6").style.visibility = "visible";
+}, 5000)
+
+setTimeout(function showdiv() {
+  document.getElementById("p7").style.visibility = "visible";
+}, 6500)
+
+setTimeout(function showdiv() {
+  document.getElementById("p8").style.visibility = "visible";
+}, 8000)
+
+setTimeout(function showdiv() {
+  document.getElementById("p9").style.visibility = "visible";
+}, 9500)
